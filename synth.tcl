24909e29d68f968231a7bf35259e8aba5186bd29
Tai Thai <tai.thai108k23@hcmut.edu.vn>
2026-01-27 19:43:34 +0700

    Add files via upload

---
+++ synth.tcl
@@ -0,0 +1,19 @@
+set_db init_lib_search_path {/home/yellow/ee3201_16/LAB2/gpdk045_lib}
+set_db init_hdl_search_path {/home/yellow/ee3201_16/LAB2/Exp2/00_src}
+read_libs slow_vdd1v2_basicCells_hvt.lib
+read_hdl -sv alu.sv
+elaborate
+set_db syn_generic_effort medium
+set_db syn_map_effort medium
+set_db syn_opt_effort medium
+syn_generic
+syn_map
+#syn_opt ---> We do not use this command in this experiment
+
+report_timing 	> 02_reports/timing.rpt
+report_power 	> 02_reports/power.rpt
+report_area 	> 02_reports/area.rpt
+report_qor 	> 02_reports/qor.rpt
+
+write_hdl 	> 03_outputs/alu_netlist.sv
+write_sdf -timescale ns -nonegchecks -recrem split -edges check_edge -setuphold split > 03_outputs/delay.sdf
